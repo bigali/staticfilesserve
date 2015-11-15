55e334ae4fb91797814360d5f898e514e8a4dba3
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-11-15 20:23:56 +0100

    add get somthing function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var getSomething = function () {
-    localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+"#"+document.getElementsByClassName('top-card-header-headline')[0].innerText+"#"+document.getElementsByClassName('profile-photo person120')[0].src+"#"+document.getElementsByClassName('summary-cell-text1')[0].innerText);
+    localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText);
 };
 
