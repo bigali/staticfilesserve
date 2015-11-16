5ca62d5f1057ae45ef76aeeaafdad8082ea82251
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-11-16 19:22:21 +0100

    add linkedin url

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var getSomething = function (url) {
-    localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText+'#'+url);
+    //localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText);
+    localStorage.setItem('name', document.getElementsByClassName('top-card-header-name')[0].innerText);
+    localStorage.setItem('title',document.getElementsByClassName('top-card-header-headline')[0].innerText);
+    localStorage.setItem('imgUrl',document.getElementsByClassName('profile-photo person120')[0].src);
+    localStorage.setItem('company',document.getElementsByClassName('summary-cell-text1')[0].innerText);
+    localStorage.setItem('linkedInUrl',document.getElementsByClassName('summary-cell-text1')[0].innerText);
 };
 
