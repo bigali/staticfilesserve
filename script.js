757cf062fa3b637b3b58b2930f7a0135cd822835
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-11-16 14:37:32 +0100

    add linkedin url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var getSomething = function (url) {
-    console.log(url);
-    localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText);
+    localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText+'#'+url);
+    console.log('url:',url)
 };
 
