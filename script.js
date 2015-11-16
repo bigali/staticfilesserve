9b0505e1fff1dac75cfb1f294da9b9bcc15d908a
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-11-16 16:20:27 +0100

    add linkedin url

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var getSomething = function (url) {
     localStorage.setItem('info', document.getElementsByClassName('top-card-header-name')[0].innerText+'#'+document.getElementsByClassName('top-card-header-headline')[0].innerText+'#'+document.getElementsByClassName('profile-photo person120')[0].src+'#'+document.getElementsByClassName('summary-cell-text1')[0].innerText+'#'+url);
-    var infos={
-        name: document.getElementsByClassName('top-card-header-name')[0].innerText,
-        title: document.getElementsByClassName('top-card-header-headline')[0].innerText,
-        imageUrl : document.getElementsByClassName('profile-photo person120')[0].src,
-        company: document.getElementsByClassName('summary-cell-text1')[0].innerText,
-        linkedinUrl: url
-    };
+    var infos=[
+        document.getElementsByClassName('top-card-header-name')[0].innerText,
+        document.getElementsByClassName('top-card-header-headline')[0].innerText,
+        document.getElementsByClassName('profile-photo person120')[0].src,
+        document.getElementsByClassName('summary-cell-text1')[0].innerText,
+        url
+    ];
     localStorage.setItem('infos',infos);
     console.log('url:',url);
     console.log('info;',infos);
