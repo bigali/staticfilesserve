665142afd62208ea272a6fcc1df7c6b2d5cf8c80
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-11-18 09:45:11 +0100

    add linkedin url

--- getinfo.js
+++ getinfo.js
@@ -1,4 +1,13 @@
 var getInfo = function () {
-    return localStorage.getItem('info');
+    var info=localStorage.getItem('info').split('#');
+    console.log(info);
+    return   {
+        'firstname': info[0],
+        'lastname': info[1],
+        'title': info[2],
+        'profile_img_url': info[3],
+        'company': info[4],
+        'linkedin_url': 'https://www.linkedin.com/profile/view?id=' + info[5]
+    };
 };
 
--- putinfo.js
+++ putinfo.js
@@ -1,10 +1,9 @@
 var putInfo = function (url) {
-    localStorage.setItem('info', {
-        name: document.getElementsByClassName('top-card-header-name')[0].innerText,
-        title: document.getElementsByClassName('top-card-header-headline')[0].innerText,
-        imgUrl: document.getElementsByClassName('profile-photo person120')[0].src,
-        company: document.getElementsByClassName('summary-cell-text1')[0].innerText,
-        linkedInUrl: url
-    })
+    var fullname=document.getElementsByClassName('top-card-header-name')[0].innerText.split(' ');
+    var title=document.getElementsByClassName('top-card-header-headline')[0].innerText;
+    var imgUrl=document.getElementsByClassName('profile-photo person120')[0].src;
+    var company=document.getElementsByClassName('summary-cell-text1')[0].innerText;
+    var linkedinMember=url.match(new RegExp("profile/(.*)/name"))[1];
+    localStorage.setItem('info',fullname[0]+'#'+fullname[1]+'#'+title+'#'+imgUrl+'#'+company+'#'+linkedinMember);
 };
 
