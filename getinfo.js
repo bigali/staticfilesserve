366336b7a8eeb685181cad623336f008624c22a4
Sidali HALLAK <sidali@Sidalis-MacBook-Pro.local>
2015-12-27 13:50:58 +0100

    extracting info from the new linked in

--- getinfo.js
+++ getinfo.js
@@ -1,22 +1,49 @@
 var getInfo = function () {
     var info=localStorage.getItem('info').split('#');
+    console.log(info);
+    var emails=[];
+    if(info[6]) {
+        emails.push({
+            "email": info[6]
+        });
+    };
+    var phones=[];
+    if(info[7]) {
+        phones.push({
+            "number": info[7]
+        });
+    };
     return {
         'firstname': info[0],
         'lastname': info[1],
         'title': info[2],
         'profile_img_url': info[3],
         'company': info[4],
-        'linkedin_url': 'https://www.linkedin.com/m/profile/' + info[5]
+        'linkedin_url': 'https://www.linkedin.com/profile/view?id=' + info[5],
+        'emails':emails,
+        'phones':phones
     };
 };
 
 var putInfo = function (url) {
-    var fullname=document.getElementsByClassName('detail name')[0].innerText.split(' ');
-    var titleCompany=document.getElementsByClassName('detail position')[0].innerText.split(' at ');
-    var title =titleCompany[0];
-    var company=titleCompany[1];
-    var imgUrl=document.getElementsByClassName('member-photo')[0].querySelectorAll('img')[0].src;;
-    var linkedinMember=url.split('/')[5];
+    var fullname=document.getElementsByClassName('top-card-header-name')[0].innerText.split(' ');
+    var title=document.getElementsByClassName('top-card-header-headline')[0].innerText;
+    var imgUrl=document.getElementsByClassName('profile-photo person120')[0].src;
+    var company=document.getElementsByClassName('summary-cell-text1')[0].innerText;
+    var linkedinMember=url.match(new RegExp("profile/(.*)/name"))[1];
+    var email='';
+    var phone='';
+    var s=document.getElementsByClassName('profile-media-object');
+    var phonePatern=/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i;
 
-    localStorage.setItem('info',fullname[0]+'#'+fullname[1]+'#'+title+'#'+imgUrl+'#'+company+'#'+linkedinMember);
+    for (i = 0; i < s.length; i++) {
+        if(s[i].children[0].nodeName=='H3'){
+            if(s[i].children[1].innerText.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) email=s[i].children[1].innerText;
+            if(s[i].children[1].innerText.split(' (')[0].match(phonePatern)) phone=s[i].children[1].innerText;
+        }
+
+    }
+    console.log('email:'+email+','+'phone:'+phone);
+
+    localStorage.setItem('info',fullname[0]+'#'+fullname[1]+'#'+title+'#'+imgUrl+'#'+company+'#'+linkedinMember+'#'+email+'#'+phone);
 };
---
+++ getinfoandroid.js
@@ -0,0 +1,22 @@
+var getInfo = function () {
+    var info=localStorage.getItem('info').split('#');
+    return {
+        'firstname': info[0],
+        'lastname': info[1],
+        'title': info[2],
+        'profile_img_url': info[3],
+        'company': info[4],
+        'linkedin_url': 'https://www.linkedin.com/m/profile/' + info[5]
+    };
+};
+
+var putInfo = function (url) {
+    var fullname=document.getElementsByClassName('detail name')[0].innerText.split(' ');
+    var titleCompany=document.getElementsByClassName('detail position')[0].innerText.split(' at ');
+    var title =titleCompany[0];
+    var company=titleCompany[1];
+    var imgUrl=document.getElementsByClassName('member-photo')[0].querySelectorAll('img')[0].src;;
+    var linkedinMember=url.split('/')[5];
+
+    localStorage.setItem('info',fullname[0]+'#'+fullname[1]+'#'+title+'#'+imgUrl+'#'+company+'#'+linkedinMember);
+};
