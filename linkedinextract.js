3b487a27a3945c3c2d397f40b452b2c04183d44c
sidali hallak <bigalitux@gmail.com>
2019-04-27 02:30:12 +0200

    Update linkedinextract.js

--- linkedinextract.js
+++ linkedinextract.js
@@ -3,9 +3,9 @@ var basics = {
     "label": extractText("pv-top-card-section__headline t-16 t-black t-normal"),
     "picture": extractText("photo-upload-preview", "src"),
     "email":extractText("pv-contact-info__contact-link t-14 t-black t-normal", "textContent", 1),
-    "phone": "(912) 555-4321",
-    "website": "http://johndoe.com",
-    "summary": "",
+    "phone": "",
+    "website": "",
+    "summary": extractText("pv-top-card-section__summary-text mt4 ember-view"),
     "profiles": [{
       "network": "linkedin",
       "username": extractText("pv-contact-info__contact-link t-14 t-black t-normal") ? extractText("pv-contact-info__contact-link t-14 t-black t-normal").split("in/")[1] : "" ,
