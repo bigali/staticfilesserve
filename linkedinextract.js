455a1ae747019fcc8b3fa6553c441bf085111e08
sidali hallak <bigalitux@gmail.com>
2019-04-27 20:00:20 +0200

    Update linkedinextract.js

--- linkedinextract.js
+++ linkedinextract.js
@@ -1,22 +1,23 @@
+
 var basics = {
     "name": extractText("pv-top-card-section__name t-24 t-black t-normal"),
     "label": extractText("pv-top-card-section__headline t-16 t-black t-normal"),
-    "picture": extractText("photo-upload-preview", "src"),
-    "email":extractText("pv-contact-info__contact-link t-14 t-black t-normal", "textContent", 1),
+    "picture": extractText("photo-upload-preview", "src") || extractText("pv-top-card-section__photo presence-entity__image EntityPhoto-circle-8 ember-view", "style").backgroundImage.split('url("')[1].split('")')[0],
+    "email":extractText("pv-contact-info__contact-link t-14 t-black t-normal","textContent", document.getElementsByClassName("pv-contact-info__contact-type ci-websites")[0]),
     "phone": "",
-    "website": "",
+    "website": extractText("pv-contact-info__contact-link t-14 t-black t-normal","textContent", document.getElementsByClassName("pv-contact-info__contact-type ci-websites")[0]),
     "summary": extractText("pv-top-card-section__summary-text mt4 ember-view"),
     "profiles": [{
       "network": "linkedin",
-      "username": extractText("pv-contact-info__contact-link t-14 t-black t-normal") ? extractText("pv-contact-info__contact-link t-14 t-black t-normal").split("in/")[1] : "" ,
-      "url": extractText("pv-contact-info__contact-link t-14 t-black t-normal")
+      "username": extractText("pv-contact-info__contact-link t-14 t-black t-normal","textContent", document.getElementsByClassName("pv-contact-info__contact-type ci-vanity-url")[0]).split("in/")[1],
+      "url": extractText("pv-contact-info__contact-link t-14 t-black t-normal","textContent", document.getElementsByClassName("pv-contact-info__contact-type ci-vanity-url")[0])
     }]
 }
 
-const extractText = (className, atr="textContent", index=0) => {
-    let elt = document.getElementsByClassName(className)
+var extractText = (className, atr="textContent", parent=document) => {
+    let elt = parent.getElementsByClassName(className)
 	if(elt) {
-		let t = elt[index]
+		let t = elt[0]
 		if(t) {
 			return t[atr]
 		} else {
